50db8901ec84bc71f7845c3245bbf4c9a385abb9
davidazv <a01665896@tec.mx>
2026-05-14 19:19:22 -0600

    fix: remove incompatible bus-model CRUD files brought from main
    These use cases (CreateBusModelUseCase, DeleteBusModelUseCase,
    UpdateBusModelUseCase, RecommendBusCountUseCase) and FleetResource
    were written against an older version of BusModelRepository and
    AfluenciaMetrobusRepository that does not match develop's current
    interface signatures, causing compilation failures.
    
    Keeping: Dockerfile, cloudbuild.yaml, docs.

--- src/main/java/org/acme/application/exception/BusModelAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package org.acme.application.exception;
-
-public class BusModelAlreadyExistsException extends RuntimeException {
-    public BusModelAlreadyExistsException(String name) {
-        super("Ya existe un modelo de bus con el nombre: " + name);
-    }
-}
--- src/main/java/org/acme/application/exception/DemandNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.acme.application.exception;
-
-public class DemandNotFoundException extends RuntimeException {
-    public DemandNotFoundException(String linea) {
-        super("No se encontraron datos de demanda para: " + linea);
-    }
-}
